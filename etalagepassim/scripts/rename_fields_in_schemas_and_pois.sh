e10ca3dd1d926f536cd59a2e44b775ecee9b4f37
Romain Soufflet <rsoufflet@easter-eggs.com>
2013-11-12 16:12:13 +0100

    Rename script.

--- etalagepassim/scripts/rename_fields_in_schemas_and_pois.sh
+++ etalagepassim/scripts/rename_fields_in_schemas_and_pois.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Offre de transport" -i "select"  -l "Couverture territoriale"    -nl "Niveau"            $1
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Application mobile" -i "url"     -l "URL Web mobile"             -nl "Web mobile"        $1
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Application mobile" -i "url"     -l "URL système Android"        -nl "Android"           $1
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Application mobile" -i "url"     -l "URL système Blackberry"     -nl "Blackberry"        $1
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Application mobile" -i "url"     -l "URL système Symbian"        -nl "Symbian"           $1
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Application mobile" -i "url"     -l "URL système Windows mobile" -nl "Windows mobile"    $1
-python etalagepassim/scripts/update_db_change_couverture_territorial_into_niveau.py -v -s "Application mobile" -i "url"     -l "URL système iPhone"         -nl "iPhone"            $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Offre de transport"      -i "select"         -l "Couverture territoriale"    -nl "Niveau"            $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Application mobile"      -i "url"            -l "URL Web mobile"             -nl "Web mobile"        $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Application mobile"      -i "url"            -l "URL système Android"        -nl "Android"           $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Application mobile"      -i "url"            -l "URL système Blackberry"     -nl "Blackberry"        $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Application mobile"      -i "url"            -l "URL système Symbian"        -nl "Symbian"           $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Application mobile"      -i "url"            -l "URL système Windows mobile" -nl "Windows mobile"    $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Application mobile"      -i "url"            -l "URL système iPhone"         -nl "iPhone"            $1
+python etalagepassim/scripts/update_db_change_field_name.py -v -s "Service d'information"   -i "territories"    -l "Territoire couvert"         -nl "Territoires"       $1
