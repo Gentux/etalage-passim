0321096fb56f35db60daa7730bb52e29364d5328
Valery Febvre <vfebvre@easter-eggs.com>
2014-07-24 13:50:57 +0200

    Cleanup

--- etalagepassim/captcha.py
+++ etalagepassim/captcha.py
@@ -1,17 +1,42 @@
 # -*- coding: utf-8 -*-
 
+
+# Etalage-Passim -- Customization of Etalage for Passim
+# By: Emmanuel Raviart <eraviart@easter-eggs.com>
+#
+# Copyright (C) 2011, 2012, 2013 Easter-eggs
+# http://gitorious.org/passim/etalage-passim
+#
+# This file is part of Etalage-Passim.
+#
+# Etalage-Passim is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Etalage-Passim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""Google reCaptcha submit helper"""
+
+
 import logging
 
 from biryani.baseconv import cleanup_line, pipe, not_none
 from recaptcha.client import captcha
 
-from . import conf, contexts
+from . import conf
 
 log = logging.getLogger(__name__)
 
 
 def submit(req):
-    ctx = contexts.Ctx(req)
     data = req.params
     recaptcha_challenge_field, error = pipe(cleanup_line, not_none)(data['recaptcha_challenge_field'])
     if error:
